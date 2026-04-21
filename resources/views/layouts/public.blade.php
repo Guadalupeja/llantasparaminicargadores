5c9e3a05fa42965928b53e6b6edd1750c503b2f0
Guadalupe Juárez Arias <lupita_ja_93@hotmail.com>
2026-04-21 12:08:44 -0600

    Agregar favicon corregido ok

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -13,10 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-<link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
 
     <title>{{ $seoTitle }}</title>
     <meta name="description" content="{{ $seoDescription }}">
